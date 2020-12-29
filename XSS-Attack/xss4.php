c819936d7e9f782821ed300e444d5e5673674350
eastmountyxz <53172375+eastmountyxz@users.noreply.github.com>
2020-12-29 09:59:07 +0800

    Add files via upload

---
+++ XSS-Attack/xss4.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>FOX-XSS闯关-第四关</title>
+</head>
+<body>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<style type="text/css">
+body,td,th {
+	color: #F00;
+}
+</style>
+
+<hr>
+<h1>FOX-XSS闯关-第四关</h1><hr>
+<h3>提示：以get传递name参数名，如xxs4.php?name=fox<br>
+任务要求：<br>
+绕过过滤,成功执行js脚本即成功<br>
+靶场说明：通过xss绕过技术执行恶意脚本<br>
+<a href="https://wpa.qq.com/msgrd?v=1&uin=79335929&site=houdao.com&menu=yes">联系作者</a>
+</strong>
+<?php
+header("Content-Type: text/html; charset=utf-8");
+if (preg_match('/script/i', $_GET["name"])) {
+	die("error");
+}
+?>
+
+Hello <?php echo $_GET["name"]; ?>
+
+</body>
+</html>
